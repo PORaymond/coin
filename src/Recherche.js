dba4136b6769f6d2cb0203a63c64a318ea16640d
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-21 16:23:38 -0500

    Ajouter les éléments du UI

--- src/Recherche.js
+++ src/Recherche.js
@@ -1,11 +1,33 @@
 import {Button, Select} from "semantic-ui-react;"
-import {Component} from "react";
-
-
+import { Component } from "react";
 class Recherche extends Component {
    render(){
-      return(<div>Barre de recherche</div>)
+
+      const optionsDpt = [
+         { value: "44", key: "44", text:"Loire Atlantique"},
+         { value: "49", key: "49", text:"Maine et Loire"},
+         { value: "53", key: "53", text:"Mayenne"},
+         { value: "72", key: "72", text:"Sarthe"},
+         { value: "85", key: "85", text:"Vendée"},
+      ];
+
+
+      const optionsType = [
+         { value: "cpam", key:"cpam", text: "Caisse primaire d'assurance maladie"},
+         { value: "cci", key:"cci", text: "Chambre de commerce et d'industrie"},
+         { value: "crous", key:"crous", text: "Crous et ses antennes"}
+      ]
+
+
+      return(
+         <div className="recherche">
+            <Select placeholder="Choisissez un département" options={optionsDpt}  />
+            <Select placeholder="Choisissez une administration"  options={optionsType}  />
+            <Button primary> Lancer la recherche </Button>
+            <Button secondary> Vider la recherche </Button>
+
+         </div>
+      )
    }
 }
-
 export default Recherche;
