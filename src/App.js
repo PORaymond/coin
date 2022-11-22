49d5b646f8499c05bc6ff47e16d16049e79b3ff0
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-21 20:44:47 -0500

    ajouter le fetch à l’api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import Recherche from "./Composants/Recherche";
 import {Component} from "react";
 
 class App extends Component {
-state = {data: {}, error: ''}
+state = {data: 0, error: ''}
 
-   onCalculer = (a, b) => {
-      console.log(a, b)
+   onCalculer = async (a, b) => {
+      if (a&&b){
+         try {
+
+            let reponse = await fetch('https://api.coingecko.com/api/v3/simple/price?ids='+ a +'&vs_currencies='+b);
+            let donnee = await reponse.json();
+            console.log (a);
+            console.log (b);
+            console.log(donnee[a][b])
+            this.setState({data:donnee[a][b]});
+            console.log( this.state.data);
+
+         } catch (e ){
+            console.log("Erreur de connexion")
+         }
+      }
    }
    render() {
       return (
